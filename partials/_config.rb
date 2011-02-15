c78f5d75a0d76665fae375df214c8b6ce45fd8be
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-02-15 15:21:13 -0500

    Move config.generators stuff to _config.rb

---
+++ partials/_config.rb
@@ -0,0 +1,9 @@
+puts "Some updates to config/applicatin.rb ... ".magenta
+generators = <<-RUBY
+  config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+RUBY
+application generators
--- partials/_rspec.rb
+++ partials/_rspec.rb
@@ -6,11 +6,3 @@ remove_dir 'test'
 
 # generate 'rspec:install'
 run "#{@rvm} exec rails generate rspec:install"
-
-generators = <<-RUBY
-  config.generators do |g|
-      g.test_framework :rspec, :fixture => true, :views => false
-      g.fixture_replacement :factory_girl, :dir => "spec/factories"
-    end
-RUBY
-application generators
