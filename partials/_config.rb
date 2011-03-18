315a3b28d577680280af8b6933e54d5e13d63e8a
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-03-17 22:12:44 -0400

    I don't want Rails generating helpers automatically

--- partials/_config.rb
+++ partials/_config.rb
@@ -1,5 +1,6 @@
 puts "Some updates to config/applicatin.rb ... ".magenta
 generators = <<-RUBY
+  config.generators.helper = false
   config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, :fixture => true, :views => false
